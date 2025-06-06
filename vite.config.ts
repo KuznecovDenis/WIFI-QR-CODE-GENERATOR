302d12e5c7aaf73aa34ae6045675cf5117a9ad57
Denis Kuznetsov <kyznecov995@icloud.com>
2025-06-06 20:12:19 +0400

    setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,14 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/wifi-qr-code-generator/",
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 });
